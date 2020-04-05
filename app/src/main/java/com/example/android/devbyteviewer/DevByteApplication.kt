8c241b6c02639772217c229e6d6886929cb0f79a
Win 10 <esmailahmed511@gmail.com>
2020-04-05 09:26:02 +0200

    Dev Bytes App

---
+++ app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.example.android.devbyteviewer
+
+import android.app.Application
+import android.os.Build
+import androidx.work.*
+import com.example.android.devbyteviewer.work.RefreshDataWork
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import timber.log.Timber
+import java.util.concurrent.TimeUnit
+
+/**
+ * Override application to setup background work via WorkManager
+ */
+class DevByteApplication : Application() {
+
+    val applicationScope = CoroutineScope(Dispatchers.Default)
+    /**
+     * onCreate is called before the first screen is shown to the user.
+     *
+     * Use it to setup any background tasks, running expensive setup operations in a background
+     * thread to avoid delaying app start.
+     */
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+        delayInit()
+    }
+    fun delayInit(){
+        setupRecurringWork()
+    }
+    // making the work request and hands it off to the system
+    private fun setupRecurringWork(){
+        // specifying the requirements required to start the  Work
+        val constraints = Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.UNMETERED)
+                .setRequiresBatteryNotLow(true)
+                .setRequiresCharging(true)
+                .apply {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                        setRequiresDeviceIdle(true)
+                    }
+                }.build()
+        // making the work request to tell the OS When and How to make the Work
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWork>(1,TimeUnit.DAYS)
+                .setConstraints(constraints)
+                .build()
+        // Hand the WorkRequest to the OS
+        WorkManager.getInstance().enqueueUniquePeriodicWork(
+                RefreshDataWork.WORK_NAME,
+                ExistingPeriodicWorkPolicy.KEEP,
+                repeatingRequest
+        )
+    }
+}
